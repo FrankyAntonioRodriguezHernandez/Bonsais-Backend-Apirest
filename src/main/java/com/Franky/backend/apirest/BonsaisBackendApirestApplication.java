90ad003d2f3952132b4678ac951541e0273c80a2
Franky Rodriguez <frankyan2001@gmail.com>
2024-10-22 11:49:24 -0400

    first commit

---
+++ src/main/java/com/Franky/backend/apirest/BonsaisBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.Franky.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonsaisBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonsaisBackendApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Franky/backend/apirest/BonsaisBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Franky.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonsaisBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
